c7854453b814d67e70b49c5ef0390ce60cf2bdd0
WylnJksn <waylon.jackson@students.fhu.edu>
2025-04-10 09:48:35 -0500

    Inputs and ships attempt 1

--- main.py
+++ main.py
@@ -1,1 +1,55 @@
-#test
+p1_ships = ["Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer",]
+p2_ships = ["Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer",]
+
+player_1 = input("Place your Ships")
+print(p1_grid)
+
+player_2 = input("Place your Ships")
+print(p2_grid)
+
+
+
+
+
+
+if p1_ships == 0:
+    print("Player 2 has won the Game!!", 'Art goes here')
+elif p2_ships == 0:
+    print("Player 1 has won the Game!!", 'Art goes here')
+else:
+    print("Something has gone wrong")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
