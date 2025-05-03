134ede4d058a93156262efab67c99b05509663f5
Glmoore <garret.moore@students.fhu.edu>
2025-05-02 19:28:52 -0500

    update

--- player_grids.py
+++ player_grids.py
@@ -1,5 +1,6 @@
 #Need to add segment that reads player's inputs for placing down thier ships at the beginning of the game, and it needs to check if
 #it is valid ship placement as well. 
+import os
 
 basic_grid = ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]",
         "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]",
@@ -26,6 +27,9 @@ column_input = int(input("Please input a column number."))
 """row_index = 0"""
 """column_index = 0"""
 
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def ShipCheck():
     #This will be where the program will check to see if all segments of any ship have been hit. If so, then it will print out "You sank (opposite player)'s (specific ship)."
     #The program will then check if all of the enemy player's ships have been sunk, determining a game end or not.
@@ -33,6 +37,7 @@ def ShipCheck():
 
 def RowCheck(row_input):
     valid_row = False
+    #I know for a fact this is inefficient. But if it works it works, I guess. I just gotta not make it a habit.
     while valid_row != True:
         if row_input == "A" or row_input == "a":
             row_index = 0
@@ -111,45 +116,72 @@ def ColumnCheck(column_input, row_index):
 
 #this will check to see if the coordinate that the player is attacking is occupied by an enemy or not, indicating a hit or miss
 #it will also check if the player's already attacked there
-def coordinates_check(enemy_grid, player_attack_grid):    
+def coordinates_check(enemy_grid, player_attack_grid, row_input, column_input):    
     valid_coords = False
     while valid_coords != True:
+        row_index = RowCheck(row_input)
+        attack_index = ColumnCheck(column_input, row_index)
         if enemy_grid[attack_index] == "[ ]" or enemy_grid[attack_index] == "[#]":
             if player_attack_grid[attack_index] == "[X]" or player_attack_grid[attack_index] == "[O]":
                 print("Please input coordinates that haven't been attacked yet.")
                 row_input = input("Please input a row letter.")
-                row_index = RowCheck(row_input)
                 column_input = input("Please input a column number.")
-                attack_index = ColumnCheck(column_input, row_index)
             else:
                 valid_coords = True
         else:
             print("Please input valid coordinates.")
             row_input = input("Please input a row letter.")
-            row_index = RowCheck(row_input)
-            column_input = input("Please input a column number.")
-            attack_index = ColumnCheck(column_input, row_index)
+            column_input = int(input("Please input a column number."))
     return attack_index
 
 
-#Still need to make it alternate between the two attacking until someone wins. I also need to make it print off each player's normal and attack grid each turn, then clear the terminal after their turn.
-#p1 attack sequence
-attack_index = coordinates_check(p2_grid, p1_attack_grid)
-if p2_grid[attack_index] == "[ ]":
-    print() #insert "miss" message
-    p1_attack_grid[attack_index] = "[O]"
-else:
-    print() #insert hit message
-    p1_attack_grid[attack_index] = "[X]"
-    p2_grid[attack_index] = "[X]"
-ShipCheck()
-#p2 attack sequence
-attack_index = coordinates_check(p1_grid, p2_attack_grid)
-if p1_grid[attack_index] == "[ ]":
-    print() #insert "miss" message
-    p2_attack_grid[attack_index] = "[O]"
-else:
-    print() #insert hit message
-    p2_attack_grid[attack_index] = "[X]"
-    p1_grid[attack_index] = "[X]"
-ShipCheck()
+def checkWinner(grid):
+    functional_part = "[#]"
+    ship_check = [i for i in grid if i == functional_part]
+    return len(ship_check) > 0
+
+#Need to input the variable's that store each player's names into the code below.
+while True:
+    #p1 attack sequence
+    print("""player1 name variable""" + "'s turn.")
+    print("Your grid:")
+    print(p1_grid)
+    print("Attack grid:")
+    print(p1_attack_grid)
+    row_input = input("Please input a row letter.")
+    column_input = int(input("Please input a column number."))
+    attack_index = coordinates_check(p2_grid, p1_attack_grid, row_input, column_input)
+    if p2_grid[attack_index] == "[ ]":
+        print() #insert "miss" message  and ascii art
+        p1_attack_grid[attack_index] = "[O]"
+    else:
+        print() #insert hit message  and ascii art
+        p1_attack_grid[attack_index] = "[X]"
+        p2_grid[attack_index] = "[X]"
+    ShipCheck()
+    if not checkWinner(p2_grid):  # Check if Player 2 has lost
+        print("""player1 name variable""" + " wins!")
+        break
+    clear_terminal()
+    
+    #p2 attack sequence
+    print("""player2 name variable""" + "'s turn.")
+    print("Your grid:")
+    print(p2_grid)
+    print("Attack grid:")
+    print(p2_attack_grid)
+    row_input = input("Please input a row letter.")
+    column_input = int(input("Please input a column number."))
+    attack_index = coordinates_check(p1_grid, p2_attack_grid, row_input, column_input)
+    if p1_grid[attack_index] == "[ ]":
+        print() #insert "miss" message and ascii art
+        p2_attack_grid[attack_index] = "[O]"
+    else:
+        print() #insert hit message and ascii art
+        p2_attack_grid[attack_index] = "[X]"
+        p1_grid[attack_index] = "[X]"
+    ShipCheck()
+    if not checkWinner(p1_grid):  # Check if Player 1 has lost
+        print("""Player2 name variable""" + " wins!")
+        break
+    clear_terminal()
